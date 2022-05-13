a990f77b10f56fc2db0e9eabd4f6b1110b58098a
Lincoln <thyssenkrupp234@gmail.com>
2022-05-13 08:09:40 -0700

    Create backup directory and cleanup code

--- main.js
+++ main.js
@@ -4,23 +4,6 @@ var __importDefault = (this && this.__importDefault) || function (mod) {
 };
 Object.defineProperty(exports, "__esModule", { value: true });
 const fs_1 = __importDefault(require("fs"));
-// if we are on MacOS, we need to run the platform specific commands, using process.platform
-if (process.platform === 'darwin') {
-    var oldPath = '/Applications/Roblox.app/Contents/Resources/content/textures/ArrowCursor.png';
-    var newPath = '/Applications/Roblox.app/Contents/Resources/content/textures/Cursors/KeyboardMouse/ArrowCursor.png';
-    fs_1.default.rename(oldPath, newPath, function (err) {
-        if (err)
-            throw err;
-        console.log('Moved ' + oldPath + ' to ' + newPath);
-    });
-    var oldPath = '/Applications/Roblox.app/Contents/Resources/content/textures/ArrowFarCursor.png';
-    var newPath = '/Applications/Roblox.app/Contents/Resources/content/textures/Cursors/KeyboardMouse/ArrowFarCursor.png';
-    fs_1.default.rename(oldPath, newPath, function (err) {
-        if (err)
-            throw err;
-        console.log('Moved ' + oldPath + ' to ' + newPath);
-    });
-}
 // if we are on Windows, we need to run the platform specific commands, using process.platform
 // windows is not officially supported, due to ever changing versions. It is planned to be supported in the future.
 //if (process.platform === 'win32') {
@@ -37,3 +20,55 @@ if (process.platform === 'darwin') {
 //    console.log('Moved ' + oldPath + ' to ' + newPath)
 //  })
 //}
+// if we are on MacOS, we need to run the platform specific commands, using process.platform
+function oldCursorMacOS() {
+    var backupDirectory = '/Applications/Roblox.app/Contents/Resources/content/textures/cursorBackup/';
+    var oldPath = '/Applications/Roblox.app/Contents/Resources/content/textures/Cursors/KeyboardMouse/ArrowCursor.png';
+    var newPath = '/Applications/Roblox.app/Contents/Resources/content/textures/cursorBackup/ArrowCursor.png';
+    if (!fs_1.default.existsSync(backupDirectory)) {
+        fs_1.default.mkdirSync(backupDirectory);
+    }
+    fs_1.default.rename(oldPath, newPath, function (err) {
+        if (err)
+            throw err;
+        console.log('Moved ' + oldPath + ' to ' + newPath);
+    });
+    console.log("Backed up default cursor to: " + newPath);
+    var backupDirectory = '/Applications/Roblox.app/Contents/Resources/content/textures/cursorBackup/';
+    var oldPath = '/Applications/Roblox.app/Contents/Resources/content/textures/Cursors/KeyboardMouse/ArrowFarCursor.png';
+    var newPath = '/Applications/Roblox.app/Contents/Resources/content/textures/cursorBackup/ArrowFarCursor.png';
+    if (!fs_1.default.existsSync(backupDirectory)) {
+        fs_1.default.mkdirSync(backupDirectory);
+    }
+    fs_1.default.rename(oldPath, newPath, function (err) {
+        if (err)
+            throw err;
+        console.log('Moved ' + oldPath + ' to ' + newPath);
+    });
+    console.log("Backed up default cursor to: " + newPath);
+    var oldPath = '/Applications/Roblox.app/Contents/Resources/content/textures/ArrowCursor.png';
+    var newPath = '/Applications/Roblox.app/Contents/Resources/content/textures/Cursors/KeyboardMouse/ArrowCursor.png';
+    fs_1.default.rename(oldPath, newPath, function (err) {
+        if (err)
+            throw err;
+        console.log('Moved ' + oldPath + ' to ' + newPath);
+    });
+    var oldPath = '/Applications/Roblox.app/Contents/Resources/content/textures/ArrowFarCursor.png';
+    var newPath = '/Applications/Roblox.app/Contents/Resources/content/textures/Cursors/KeyboardMouse/ArrowFarCursor.png';
+    fs_1.default.rename(oldPath, newPath, function (err) {
+        if (err)
+            throw err;
+        console.log('Moved ' + oldPath + ' to ' + newPath);
+    });
+}
+if (process.argv[2] && process.platform === 'darwin' && process.argv[2] === '--revert') {
+    console.log('Reverting to default cursor');
+}
+else {
+    console.log('No arguments passed, defaulting to new cursor');
+    oldCursorMacOS();
+}
+if (process.platform !== 'darwin') {
+    console.log('This script is only supported on MacOS, exiting.');
+    process.exit();
+}
--- main.ts
+++ main.ts
@@ -1,20 +1,8 @@
 import fs from 'fs';
 
-// if we are on MacOS, we need to run the platform specific commands, using process.platform
-if (process.platform === 'darwin') {
-  var oldPath = '/Applications/Roblox.app/Contents/Resources/content/textures/ArrowCursor.png';
-  var newPath = '/Applications/Roblox.app/Contents/Resources/content/textures/Cursors/KeyboardMouse/ArrowCursor.png';
-  fs.rename(oldPath, newPath, function (err) {
-    if (err) throw err  
-    console.log('Moved ' + oldPath + ' to ' + newPath)
-  })
-  var oldPath = '/Applications/Roblox.app/Contents/Resources/content/textures/ArrowFarCursor.png';
-  var newPath = '/Applications/Roblox.app/Contents/Resources/content/textures/Cursors/KeyboardMouse/ArrowFarCursor.png'; 
-  fs.rename(oldPath, newPath, function (err) {
-    if (err) throw err
-    console.log('Moved ' + oldPath + ' to ' + newPath)
-  })
-}
+
+
+
 
 // if we are on Windows, we need to run the platform specific commands, using process.platform
 // windows is not officially supported, due to ever changing versions. It is planned to be supported in the future.
@@ -32,3 +20,57 @@ if (process.platform === 'darwin') {
 //    console.log('Moved ' + oldPath + ' to ' + newPath)
 //  })
 //}
+
+// if we are on MacOS, we need to run the platform specific commands, using process.platform
+
+function oldCursorMacOS() {
+  var backupDirectory = '/Applications/Roblox.app/Contents/Resources/content/textures/cursorBackup/';
+  var oldPath = '/Applications/Roblox.app/Contents/Resources/content/textures/Cursors/KeyboardMouse/ArrowCursor.png';
+  var newPath = '/Applications/Roblox.app/Contents/Resources/content/textures/cursorBackup/ArrowCursor.png';
+  if (!fs.existsSync(backupDirectory)){
+    fs.mkdirSync(backupDirectory);
+}
+  fs.rename(oldPath, newPath, function (err) {
+    if (err) throw err  
+    console.log('Moved ' + oldPath + ' to ' + newPath)
+  })
+  console.log("Backed up default cursor to: " + newPath);
+  var backupDirectory = '/Applications/Roblox.app/Contents/Resources/content/textures/cursorBackup/';
+  var oldPath = '/Applications/Roblox.app/Contents/Resources/content/textures/Cursors/KeyboardMouse/ArrowFarCursor.png';
+  var newPath = '/Applications/Roblox.app/Contents/Resources/content/textures/cursorBackup/ArrowFarCursor.png';
+  if (!fs.existsSync(backupDirectory)){
+    fs.mkdirSync(backupDirectory);
+}
+  fs.rename(oldPath, newPath, function (err) {
+    if (err) throw err  
+    console.log('Moved ' + oldPath + ' to ' + newPath)
+  })
+  console.log("Backed up default cursor to: " + newPath);
+  var oldPath = '/Applications/Roblox.app/Contents/Resources/content/textures/ArrowCursor.png';
+  var newPath = '/Applications/Roblox.app/Contents/Resources/content/textures/Cursors/KeyboardMouse/ArrowCursor.png';
+  fs.rename(oldPath, newPath, function (err) {
+    if (err) throw err  
+    console.log('Moved ' + oldPath + ' to ' + newPath)
+  })
+  var oldPath = '/Applications/Roblox.app/Contents/Resources/content/textures/ArrowFarCursor.png';
+  var newPath = '/Applications/Roblox.app/Contents/Resources/content/textures/Cursors/KeyboardMouse/ArrowFarCursor.png'; 
+  fs.rename(oldPath, newPath, function (err) {
+    if (err) throw err
+    console.log('Moved ' + oldPath + ' to ' + newPath)
+})
+}
+
+
+
+
+if (process.argv[2] && process.platform === 'darwin' && process.argv[2] === '--revert') {
+  console.log('Reverting to default cursor');
+} else {
+  console.log('No arguments passed, defaulting to new cursor');
+  oldCursorMacOS();
+}
+
+if (process.platform !== 'darwin') {
+  console.log('This script is only supported on MacOS, exiting.');
+  process.exit();
+}
