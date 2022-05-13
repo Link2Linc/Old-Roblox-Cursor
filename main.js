e0bd351d278f32df8c2d7eaa209e7b91c4691644
Lincoln <thyssenkrupp234@gmail.com>
2022-05-13 08:10:36 -0700

    Remove windows code

--- main.js
+++ main.js
@@ -4,22 +4,6 @@ var __importDefault = (this && this.__importDefault) || function (mod) {
 };
 Object.defineProperty(exports, "__esModule", { value: true });
 const fs_1 = __importDefault(require("fs"));
-// if we are on Windows, we need to run the platform specific commands, using process.platform
-// windows is not officially supported, due to ever changing versions. It is planned to be supported in the future.
-//if (process.platform === 'win32') {
-//  var oldPath = 'C:\\Program Files (x86)\\Roblox\\Roblox.exe';
-//  var newPath = 'C:\\Program Files (x86)\\Roblox\\Roblox.exe';
-//  fs.rename(oldPath, newPath, function (err) {
-//    if (err) throw err
-//    console.log('Moved ' + oldPath + ' to ' + newPath)
-//  })
-//  var oldPath = 'C:\\Program Files (x86)\\Roblox\\Roblox.exe';
-//  var newPath = 'C:\\Program Files (x86)\\Roblox\\Roblox.exe';
-//  fs.rename(oldPath, newPath, function (err) {
-//    if (err) throw err
-//    console.log('Moved ' + oldPath + ' to ' + newPath)
-//  })
-//}
 // if we are on MacOS, we need to run the platform specific commands, using process.platform
 function oldCursorMacOS() {
     var backupDirectory = '/Applications/Roblox.app/Contents/Resources/content/textures/cursorBackup/';
--- main.ts
+++ main.ts
@@ -1,28 +1,5 @@
 import fs from 'fs';
-
-
-
-
-
-// if we are on Windows, we need to run the platform specific commands, using process.platform
-// windows is not officially supported, due to ever changing versions. It is planned to be supported in the future.
-//if (process.platform === 'win32') {
-//  var oldPath = 'C:\\Program Files (x86)\\Roblox\\Roblox.exe';
-//  var newPath = 'C:\\Program Files (x86)\\Roblox\\Roblox.exe';
-//  fs.rename(oldPath, newPath, function (err) {
-//    if (err) throw err
-//    console.log('Moved ' + oldPath + ' to ' + newPath)
-//  })
-//  var oldPath = 'C:\\Program Files (x86)\\Roblox\\Roblox.exe';
-//  var newPath = 'C:\\Program Files (x86)\\Roblox\\Roblox.exe';
-//  fs.rename(oldPath, newPath, function (err) {
-//    if (err) throw err
-//    console.log('Moved ' + oldPath + ' to ' + newPath)
-//  })
-//}
-
 // if we are on MacOS, we need to run the platform specific commands, using process.platform
-
 function oldCursorMacOS() {
   var backupDirectory = '/Applications/Roblox.app/Contents/Resources/content/textures/cursorBackup/';
   var oldPath = '/Applications/Roblox.app/Contents/Resources/content/textures/Cursors/KeyboardMouse/ArrowCursor.png';
