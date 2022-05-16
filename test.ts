9b3f0e83b9f899966b6409e2495d224cf37055ad
thyssenkrupp234 <thyssenkrupp234@gmail.com>
2022-05-16 14:52:42 -0700

    Add @types/node, mess around with test.ts

--- test.ts
+++ test.ts
@@ -1,7 +1,20 @@
-import fs from 'fs';
+const os = require('os')
+const homefolder = os.homedir()
+const hostname = os.hostname()
+const platform = os.platform()
+const tempFiles = os.tmpdir()
+const type = os.type()
+const uptime = os.uptime()
+const verison = os.version()
+console.log("Your home-folder is" + homefolder + ". We are now putting this into a file and uploading it to our super secret server.")
 
-const folderPath = '/Users/lincolnmuller';
+console.log("Also, some more super secret information about your computer! Your hostname is " + hostname + "!")
 
-const lol = fs.readdirSync(folderPath);
+if (platform === 'win32') {
+    console.log("Fuck you, you are on Windows!")
+}
 
-console.log(lol);
+console.log("Cluttering your temporary directory with useless log files... (btw, ur temp files directory is " + tempFiles + ")")
+console.log("your OS type is " + type + "!")
+console.log("Damn. Your computer has been up for " + uptime + " seconds!")
+console.log("You are pushing your computer to the limit. Your computer is running version " + verison + "!")
