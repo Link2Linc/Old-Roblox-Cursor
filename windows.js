bc8fd34b54c1a89192896835f400d6ece7ca6427
Thyssenkrupp234 <Thyssenkrupp234@gmail.com>
2022-05-26 10:20:46 -0700

    Add full windows support

--- windows.js
+++ windows.js
@@ -1,35 +1,80 @@
 "use strict";
+// refurbished and cleanup up windows-compatible script
 var __importDefault = (this && this.__importDefault) || function (mod) {
     return (mod && mod.__esModule) ? mod : { "default": mod };
 };
 Object.defineProperty(exports, "__esModule", { value: true });
-// beta script to work on windows
-const fs_1 = __importDefault(require("fs"));
 const os_1 = __importDefault(require("os"));
-const path_1 = __importDefault(require("path"));
+const fs_1 = __importDefault(require("fs"));
 const homedir = os_1.default.homedir();
-var getMostRecent = function (dir, cb) {
-    var dir = homedir;
-    var files = fs_1.default.readdir(dir, function (err, files) {
-        var sorted = files.map(function (v) {
-            var filepath = path_1.default.resolve(dir, v);
-            return {
-                name: v,
-                time: fs_1.default.statSync(filepath).mtime.getTime()
-            };
-        })
-            .sort(function (a, b) { return b.time - a.time; })
-            .map(function (v) { return v.name; });
-        if (sorted.length > 0) {
-            cb(null, sorted[0]);
++"/";
+const robloxDir = homedir + "\\AppData\\Local\\Roblox\\Versions" + "\\";
+console.log("Roblox version directory is " + robloxDir);
+// find the newest folder in the robloxDir directory and return it
+function getLatestRobloxVersion() {
+    let folders = fs_1.default.readdirSync(robloxDir);
+    let latestVersion = folders[0];
+    for (let i = 0; i < folders.length; i++) {
+        if (folders[i] > latestVersion) {
+            latestVersion = folders[i];
         }
-        else {
-            cb('Roblox appears to not be installed. Why do you plan to restore the old Roblox cursor, when Roblox doesn\'t even exist?', null);
-        }
+    }
+    return latestVersion;
+}
+const latestVersion = getLatestRobloxVersion();
+console.log("Latest roblox version installed is" + latestVersion + ". Defulting to this version.");
+function bringBackOldCursorWin() {
+    var backupDir = robloxDir + latestVersion + "\\Content\\textures\\oldCursorBackup\\";
+    var oldCursorDir = robloxDir + latestVersion + "\\Content\\textures\\ArrowCursor.png";
+    var newDir = robloxDir + latestVersion + "\\Content\\textures\\oldCursorBackup\\ArrowCursor.png";
+    if (!fs_1.default.existsSync(backupDir)) {
+        fs_1.default.mkdirSync(backupDir);
+    }
+    ;
+    fs_1.default.rename(oldCursorDir, newDir, function (err) {
+        if (err)
+            console.log("Roblox is not installed. Please install Roblox and try again.");
+        console.log('Moved ' + oldCursorDir + ' to ' + newDir);
     });
-};
-getMostRecent('./', function (err, recent) {
-    if (err)
-        console.error(err);
-    console.log(recent);
-});
+    var oldCursorDir = robloxDir + latestVersion + "\\Content\\textures\\ArrowFarCursor.png";
+    var newDir = robloxDir + latestVersion + "\\Content\\textures\\oldCursorBackup\\ArrowFarCursor.png";
+    fs_1.default.rename(oldCursorDir, newDir, function (err) {
+        if (err)
+            console.log("Roblox is not installed. Please install Roblox and try again.");
+        console.log('Moved ' + oldCursorDir + ' to ' + newDir);
+    });
+    var cursorBackup = robloxDir + latestVersion + "\\Content\\textures\\cursorBackup";
+    var oldDir = robloxDir + latestVersion + "\\Content\\textures\\Cursors\\KeyboardMouse\\ArrowCursor.png";
+    var newDir = robloxDir + latestVersion + "\\Content\\textures\\cursorBackup\\ArrowCursor.png";
+    if (!fs_1.default.existsSync(cursorBackup)) {
+        fs_1.default.mkdirSync(cursorBackup);
+    }
+    ;
+    fs_1.default.rename(oldDir, newDir, function (err) {
+        if (err)
+            console.log("Roblox is not installed. Please install Roblox and try again.");
+        console.log('Moved ' + oldDir + ' to ' + newDir);
+    });
+    var oldDir = robloxDir + latestVersion + "\\Content\\textures\\Cursors\\KeyboardMouse\\ArrowFarCursor.png";
+    var newDir = robloxDir + latestVersion + "\\Content\\textures\\cursorBackup\\ArrowFarCursor.png";
+    fs_1.default.rename(oldDir, newDir, function (err) {
+        if (err)
+            console.log("Roblox is not installed. Please install Roblox and try again.");
+        console.log('Moved ' + oldDir + ' to ' + newDir);
+    });
+    var oldDir = robloxDir + latestVersion + "\\Content\\textures\\oldCursorBackup\\ArrowCursor.png";
+    var newDir = robloxDir + latestVersion + "\\Content\\textures\\Cursors\\KeyboardMouse\\ArrowCursor.png";
+    fs_1.default.copyFile(oldDir, newDir, function (err) {
+        if (err)
+            console.log("Roblox is not installed. Please install Roblox and try again.");
+        console.log('Copied ' + oldDir + ' to ' + newDir);
+    });
+    var oldDir = robloxDir + latestVersion + "\\Content\\textures\\oldCursorBackup\\ArrowFarCursor.png";
+    var newDir = robloxDir + latestVersion + "\\Content\\textures\\Cursors\\KeyboardMouse\\ArrowFarCursor.png";
+    fs_1.default.copyFile(oldDir, newDir, function (err) {
+        if (err)
+            console.log("Roblox is not installed. Please install Roblox and try again.");
+        console.log('Copied ' + oldDir + ' to ' + newDir);
+    });
+    console.log("Successfully completed operation. Please restart your Roblox client.");
+}
--- windows.ts
+++ windows.ts
@@ -20,4 +20,69 @@ function getLatestRobloxVersion() {
 }
 
 const latestVersion = getLatestRobloxVersion();
-console.log(latestVersion);
+console.log("Latest roblox version installed is" + latestVersion + ". Defulting to this version.");
+
+
+function bringBackOldCursorWin(){
+    var backupDir = robloxDir + latestVersion + "\\Content\\textures\\oldCursorBackup\\";
+    var oldCursorDir = robloxDir + latestVersion + "\\Content\\textures\\ArrowCursor.png";
+    var newDir = robloxDir + latestVersion + "\\Content\\textures\\oldCursorBackup\\ArrowCursor.png";
+    if (!fs.existsSync(backupDir)) {
+        fs.mkdirSync(backupDir);
+    };
+    fs.rename(oldCursorDir, newDir, function (err) {
+        if (err) console.log("Roblox is not installed. Please install Roblox and try again.");
+        console.log('Moved ' + oldCursorDir + ' to ' + newDir)
+    });
+    var oldCursorDir = robloxDir + latestVersion + "\\Content\\textures\\ArrowFarCursor.png";
+    var newDir = robloxDir + latestVersion + "\\Content\\textures\\oldCursorBackup\\ArrowFarCursor.png";
+    fs.rename(oldCursorDir, newDir, function (err) {
+        if (err) console.log("Roblox is not installed. Please install Roblox and try again.");
+        console.log('Moved ' + oldCursorDir + ' to ' + newDir)
+    });
+    var cursorBackup = robloxDir + latestVersion + "\\Content\\textures\\cursorBackup";
+    var oldDir = robloxDir + latestVersion + "\\Content\\textures\\Cursors\\KeyboardMouse\\ArrowCursor.png";
+    var newDir = robloxDir + latestVersion + "\\Content\\textures\\cursorBackup\\ArrowCursor.png";
+    if (!fs.existsSync(cursorBackup)) {
+        fs.mkdirSync(cursorBackup);
+    };
+    fs.rename(oldDir, newDir, function (err) {
+        if (err) console.log("Roblox is not installed. Please install Roblox and try again.");
+        console.log('Moved ' + oldDir + ' to ' + newDir)
+    });
+    var oldDir = robloxDir + latestVersion + "\\Content\\textures\\Cursors\\KeyboardMouse\\ArrowFarCursor.png";
+    var newDir = robloxDir + latestVersion + "\\Content\\textures\\cursorBackup\\ArrowFarCursor.png";
+    fs.rename(oldDir, newDir, function (err) {
+        if (err) console.log("Roblox is not installed. Please install Roblox and try again.");
+        console.log('Moved ' + oldDir + ' to ' + newDir)
+    });
+    var oldDir = robloxDir + latestVersion + "\\Content\\textures\\oldCursorBackup\\ArrowCursor.png";
+    var newDir = robloxDir + latestVersion + "\\Content\\textures\\Cursors\\KeyboardMouse\\ArrowCursor.png";
+    fs.copyFile(oldDir, newDir, function (err) {
+        if (err) console.log("Roblox is not installed. Please install Roblox and try again.");
+        console.log('Copied ' + oldDir + ' to ' + newDir)
+    });
+    var oldDir = robloxDir + latestVersion + "\\Content\\textures\\oldCursorBackup\\ArrowFarCursor.png";
+    var newDir = robloxDir + latestVersion + "\\Content\\textures\\Cursors\\KeyboardMouse\\ArrowFarCursor.png";
+    fs.copyFile(oldDir, newDir, function (err) {
+        if (err) console.log("Roblox is not installed. Please install Roblox and try again.");
+        console.log('Copied ' + oldDir + ' to ' + newDir)
+    });
+    console.log("Successfully completed operation. Please restart your Roblox client.");
+}
+
+function revertDefaultCursorWin(){
+    var oldCursorDir = robloxDir + latestVersion + "\\Content\\textures\\cursorBackup\\ArrowCursor.png";
+    var newDir = robloxDir + latestVersion + "\\Content\\textures\\ArrowCursor.png";
+    fs.copyFileSync(oldCursorDir, newDir);
+    var oldCursorDir = robloxDir + latestVersion + "\\Content\\textures\\cursorBackup\\ArrowFarCursor.png";
+    var newDir = robloxDir + latestVersion + "\\Content\\textures\\ArrowFarCursor.png";
+    fs.copyFileSync(oldCursorDir, newDir);
+}
+
+if (process.argv[2] && process.platform === 'win32' && process.argv[2] === '--revert') {
+    revertDefaultCursorWin();
+  } else {
+    console.log('No arguments passed, defaulting to new cursor');
+    bringBackOldCursorWin();
+  }
