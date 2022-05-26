4ed2a29ec3d4d8acfe02d49f81c3e7995a90289c
Thyssenkrupp234 <76831903+Thyssenkrupp234@users.noreply.github.com>
2022-05-26 09:51:10 -0700

    Update windows.ts

--- windows.ts
+++ windows.ts
@@ -1,31 +1,23 @@
-// beta script to work on windows
-import fs from 'fs';
+// refurbished and cleanup up windows-compatible script
+
 import os from 'os';
-import path from 'path';
-const homedir = os.homedir();
+import fs from 'fs';
+const homedir = os.homedir(); + "/"
+const robloxDir = homedir + "\\AppData\\Local\\Roblox\\Versions" + "\\";
 
-var getMostRecent = function (dir: string, cb: any) {
-	var dir = homedir
-	var files = fs.readdir(dir, function (err, files) {
-		var sorted = files.map(function(v) {
-			var filepath = path.resolve(dir, v);
-			return {
-				name:v,
-				time:fs.statSync(filepath).mtime.getTime()
-			}; 
-		})
-		.sort(function(a, b) { return b.time - a.time; })
-		.map(function(v) { return v.name; });
+console.log("Roblox version directory is " + robloxDir);
 
-		if (sorted.length > 0) {
-			cb(null, sorted[0]);
-		} else {
-			cb('Roblox appears to not be installed. Why do you plan to restore the old Roblox cursor, when Roblox doesn\'t even exist?', null);
-		}
-	})
+// find the newest folder in the robloxDir directory and return it
+function getLatestRobloxVersion() {
+    let folders = fs.readdirSync(robloxDir);
+    let latestVersion = folders[0];
+    for (let i = 0; i < folders.length; i++) {
+        if (folders[i] > latestVersion) {
+            latestVersion = folders[i];
+        }
+    }
+    return latestVersion;
 }
 
-getMostRecent('./', function (err: any, recent: any) {
-	if (err) console.error(err);
-	console.log(recent);
-});
+const latestVersion = getLatestRobloxVersion();
+console.log(latestVersion);
