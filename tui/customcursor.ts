83b2c33bf6b3963710ba5e15f9ac675d87ef7f71
Thyssenkrupp234 <Thyssenkrupp234@gmail.com>
2022-06-13 14:07:35 -0700

    Fox

--- tui/customcursor.ts
+++ tui/customcursor.ts
@@ -5,7 +5,7 @@ var rl = readline.createInterface(
     process.stdin, process.stdout);
 
 
-const custompath = rl.question('Please enter custom cursor path. Note: only .png files are supported. Just drag the file from file explorer into the terminal.\n', async (path: any) => {
+const custompath = rl.question('Please enter custom cursor path. Note: only .png files are supported. Just drag the file from file explorer into the terminal.\n', async(path: any) => {
     console.log('Custom cursor path is ' + path);
     rl.close();
     return path;
